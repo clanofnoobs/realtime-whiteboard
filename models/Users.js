7ce5f20a4ff82bcb56f27b7e69d393e75b187f8f
Md Samiul Huq <samiulg3@gmail.com>
2015-03-04 01:19:19 -0500

    local signup

--- models/Users.js
+++ models/Users.js
@@ -2,9 +2,11 @@ var mongoose = require("mongoose");
 var bcrypt = require('bcrypt-nodejs');
 
 var UserSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-  email: String
+  local : { 
+    username: String,
+    password: String,
+    email: String
+  }
 });
 
 UserSchema.methods.generateHash = function(password) {
--- passport/init.js
+++ passport/init.js
@@ -15,4 +15,27 @@ module.exports = function(passport){
   });
 
 
+  //Local signup
+  passport.use('local-signup', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+  },
+  function(req, email, password, done){
+    process.nextTick(function(){
+      User.findOne({$and:[{'local.email': email }, {'local.username': username}]}, function(err,user){
+        if (err){
+          return done(err);
+        }
+        if (user){
+          return done(null, false, req.flash('signupMessage', 'That email is already taken'));
+        } else {
+          var newUser = new User();
+          newUser.local.email = email;
+        }
+      });
+    });
+  }
+  );
+
 }
