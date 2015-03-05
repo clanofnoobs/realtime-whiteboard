a2bc574a2e865ffdc68b27a7fd345aae9979c0f5
Md Samiul Huq <samiulg3@gmail.com>
2015-03-05 12:29:57 -0500

    minor changes

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -15,17 +15,22 @@ app.controller('login', ['$scope', '$http','$timeout', function($scope, $http, $
     if ($scope.email.match(/\w+@\w+.(com|gov|org|net|\w+.edu)/)){
       $scope.getUsername($scope.email);
     } else {
-      $scope.eTaken, $scope.eNotTaken = false;
+      $scope.eTaken = false;
+      $scope.eNotTaken = false;
     }
   });
 
   $scope.getUsername = function(cred){
-    $scope.loadingUser = true;
+    if (cred == $scope.email){
+      $scope.loadingEmail = true;
+    } else {
+      $scope.loadingUser = true;
+    }
     return $http.get('/checkusername?username='+cred)
       .success(function(data){
       $timeout(function(){
         if (cred == $scope.email){
-          $scope.loadingUser = false;
+          $scope.loadingEmail= false;
           $scope.eNotTaken = true;
           $scope.eTaken = false;
         } else {
@@ -37,7 +42,7 @@ app.controller('login', ['$scope', '$http','$timeout', function($scope, $http, $
       }).error(function(data){
         $timeout(function(){
           if (cred == $scope.email){
-            $scope.loadingUser = false;
+            $scope.loadingEmail = false;
             $scope.eNotTaken = false;
             $scope.eTaken = true;
           } else {
