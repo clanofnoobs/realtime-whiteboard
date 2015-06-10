7f48f53e6dc9063123df6669e7e146e7aed310b7
MD Huq <samiulg3@gmail.com>
2015-06-10 15:08:44 +0300

    save image

--- models/Whiteboard.js
+++ models/Whiteboard.js
@@ -8,6 +8,7 @@ var WhiteboardSchema = new mongoose.Schema({
   created: { type: Date, default: Date.now() },
   author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   access: [{type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  img_url: String,
   controlled_access: [{type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
 });
 
